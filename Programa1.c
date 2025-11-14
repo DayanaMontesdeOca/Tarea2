65498bea41f9030ec47c9ec2ba936608bec59e76
DayanaMontesdeOca <fi.montesdeoca.unam@gmail.com>
2025-11-13 23:40:58 -0600

    Update Programa1.c

--- Programa1.c
+++ Programa1.c
@@ -12,22 +12,22 @@ int main()
     printf("\n");
    
     printf("Ingresa el primer número: ");
-    scanf("%f", &N1);
+    scanf("%f", &N1); //SE LEE EL PRIMER NUMERO FLOTANTE
     
     printf("Ingresa el segundo número: ");
-    scanf("%f", &N2);
+    scanf("%f", &N2); //SE LEE EL SEGUNDO NUMERO FLOTATE
     printf("\n"); 
    
-    printf("La suma de ambos número es: %.2f\n", N1 + N2);
+    printf("La suma de ambos número es: %.2f\n", N1 + N2); //SE SUMAN Y SE IMRPIME N1 +N2
     printf("\n"); 
     
-    printf("La resta del primer número menos el segundo es: %.2f\n", N1 - N2);
+    printf("La resta del primer número menos el segundo es: %.2f\n", N1 - N2); //SE RESTAN Y SE IMRPIME N1-N2
     printf("\n"); 
     
-    printf("La resta del segundo número menos el primer es: %.2f\n", N2 - N1);
+    printf("La resta del segundo número menos el primer es: %.2f\n", N2 - N1); //SE RESTAN Y SE IMRPIME N2-N1
     printf("\n"); 
     
-    printf("La multiplicacion de ambos números es: %.2f\n", N1 * N2);
+    printf("La multiplicacion de ambos números es: %.2f\n", N1 * N2); //SE MULTIPLICAN Y SE IMPRIME N1xN2
 
     return 0;
 }
