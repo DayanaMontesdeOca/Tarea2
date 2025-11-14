cb0a0f9e9fa953d3874f77dcafee823e3cedb081
DayanaMontesdeOca <fi.montesdeoca.unam@gmail.com>
2025-11-14 00:16:24 -0600

    Update Programa1.c

--- Programa1.c
+++ Programa1.c
@@ -7,25 +7,15 @@ int main()
 
 {
     float N1, N2;
-    printf("...ESTE PROGRAMA SUMA, RESTA Y MULTIPLICA DOS NÚMEROS...");
-   
+    printf("...ESTE PROGRAMA SUMA, RESTA Y MULTIPLICA DOS NÚMEROS...\n\n");   
     printf("Ingresa el primer número: ");
-    scanf("%f", &N1); //SE LEE EL PRIMER NUMERO FLOTANTE
-    
+    scanf("%f", &N1);    
     printf("Ingresa el segundo número: ");
-    scanf("%f", &N2); //SE LEE EL SEGUNDO NUMERO FLOTATE
-    printf("\n"); 
-   
-    printf("La suma de ambos número es: %.2f\n", N1 + N2); //SE SUMAN Y SE IMRPIME N1 +N2
-    printf("\n"); 
-    
-    printf("La resta del primer número menos el segundo es: %.2f\n", N1 - N2); //SE RESTAN Y SE IMRPIME N1-N2
-    printf("\n"); 
-    
-    printf("La resta del segundo número menos el primer es: %.2f\n", N2 - N1); //SE RESTAN Y SE IMRPIME N2-N1
-    printf("\n"); 
-    
-    printf("La multiplicacion de ambos números es: %.2f\n", N1 * N2); //SE MULTIPLICAN Y SE IMPRIME N1xN2
+    scanf("%f\n", &N2); 
+    printf("La suma de ambos número es: %.2f\n\n", N1 + N2); 
+    printf("La resta del primer número menos el segundo es: %.2f\n\n", N1 - N2); 
+    printf("La resta del segundo número menos el primer es: %.2f\n\n", N2 - N1);
+    printf("La multiplicacion de ambos números es: %.2f\n", N1 * N2);
 
     return 0;
 }
